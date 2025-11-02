e15f7f045b2402595d7d436dbc08092acc54a2c9
shefat2002 <shefat2002@gmail.com>
2025-11-02 22:32:07 +0600

    added 1 problem

--- Leetcode/3.Longest_Substring_Without_Repeating_Characters.cs
+++ Leetcode/3.Longest_Substring_Without_Repeating_Characters.cs
@@ -25,5 +25,25 @@ public class Solution {
     }
     
     // Approach: Sliding Window with Dictionary (O(n))
-    
+    public int LengthOfLongestSubstring(string s) {
+        //dictionary approach
+        int n = s.Length;
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        int l = 0, ans = 0, r = 0;
+        while (r < n)
+        {
+            if (dict.ContainsKey(s[r]))
+            {
+                // Console.WriteLine("Character " + s[r] + " found at index " + dict[s[r]]);
+                l = Math.Max(dict[s[r]] + 1, l);
+                // Console.WriteLine("l updated to: " + l);
+            }
+            dict[s[r]] = r;
+            // Console.WriteLine("dict updated: " + s[r] + " -> " + r);
+            ans = Math.Max(ans, r - l + 1);
+            // Console.WriteLine("ans updated to: " + ans);
+            r++;
+        }
+        return ans;
+    }
 }
